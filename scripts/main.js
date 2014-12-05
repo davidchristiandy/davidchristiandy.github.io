b7fc557de6f9abb161d5ccbf967e61309c3f489f
David Christiandy <david.christiandy@gmail.com>
2014-12-06 01:21:58 +0700

    Play around

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+// Nothing fancy.
+var wut = document.getElementById('wut')
+  , wat = document.getElementById('wat')
+  , i = 1
+  , j = 1
+  , innerDelay = 2000
+  , delay = 0;
+
+setTimeout(function() {
+  setInterval(function() {
+    wut.className = 'variation-'+i;
+    i = i % 4 + 1;
+  }, innerDelay);
+
+  setInterval(function() {
+    wat.className = 'variation-'+j;
+    j = j % 4 + 1;
+  }, innerDelay);
+}, delay);
+
